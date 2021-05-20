00db7002476d28cb933484060d28b51456a4e516
AnVolkova10 <curzi.angela@gmail.com>
2021-05-20 02:38:41 -0300

    primer commit

--- js/main-guest.js
+++ js/main-guest.js
@@ -1,5 +1,5 @@
 window.onload = () => {
     document.querySelector("form").onsubmit = function () {
-        location.href = "https://anvolkova10.github.io/lasextapata/registred.html";
+        location.href = "registred.html";
     };
 }
--- js/main-login.js
+++ js/main-login.js
@@ -1,5 +1,5 @@
 window.onload = () => {
     document.querySelector("form").onsubmit = function () {
-        location.href = "https://anvolkova10.github.io/lasextapata/index.html";
+        location.href = "index.html";
     };
 }
